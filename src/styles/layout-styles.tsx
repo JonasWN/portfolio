31371b624e068ef908aed899f0b20806090d87e1
Jonas Wessman Nielsen <5615825@RTS-365.dk>
2020-06-15 11:40:45 +0200

    Add colors, font-sizes & shadows to theme

--- src/styles/layout-styles.tsx
+++ src/styles/layout-styles.tsx
@@ -1,17 +1,17 @@
 import styled from "styled-components"
 
 const sizes = {
-  mobile: "375px",
-  tablet: "768px",
-  desktop: "1280px",
-  desktopL: "1920px",
+  sm: "375px",
+  md: "768px",
+  lg: "1280px",
+  xl: "1920px",
 }
 
 export const device = {
-  mobile: `(max-width: ${sizes.mobile})`,
-  tablet: `(max-width: ${sizes.tablet})`,
-  desktop: `(max-width: ${sizes.desktop})`,
-  desktopL: `(max-width: ${sizes.desktopL})`,
+  mobile: `(max-width: ${sizes.sm})`,
+  tablet: `(max-width: ${sizes.md})`,
+  desktop: `(max-width: ${sizes.lg})`,
+  desktopL: `(max-width: ${sizes.xl})`,
 }
 
 export const Container = styled.div`
--- src/styles/theme-styles.tsx
+++ src/styles/theme-styles.tsx
@@ -1,1 +1,28 @@
-export const Theme = {}
+export const Theme = {
+  color: {
+    background: {
+      site: "#F9FAFC",
+      container: "#0F1320",
+    },
+    font: {
+      title: "#20204B",
+      textDark: "#0F1320",
+      textLight: "#F9FAFC",
+    },
+    accents: {
+      bodo: "#FF027C",
+      beige: "#FFECA7",
+    },
+  },
+
+  fontSizes: {
+    sm: "1rem",
+    md: "1.25rem",
+    lg: "1.75rem",
+    xl: "2.50rem",
+  },
+
+  shadows: {
+    container: "1.5rem 2.5rem 5rem 0.7rem rgba(0,0,0,.13)",
+  },
+}
