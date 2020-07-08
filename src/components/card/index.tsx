322d582248926e885664a2b1eed37fb77e8ec9ef
Jonas Nielsen <jonn0512@gmail.com>
2020-07-08 13:31:13 +0200

    Add Card images

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,4 +1,10 @@
 import React from "react"
+//@ts-ignore
+import Tech from "../../static/images/cards/tech.svg"
+//@ts-ignore
+import Design from "../../static/images/cards/design.svg"
+//@ts-ignore
+import Udvikling from "../../static/images/cards/udvikling.svg"
 
 type Tprops = {
   title: string
@@ -9,7 +15,9 @@ const Card: React.FC<Tprops> = ({ title, description }) => {
   return (
     <article>
       <h4>{title}</h4>
-      <svg></svg>
+      {title === "Teknologi" && <Tech />}
+      {title === "Design" && <Design />}
+      {title === "Udvikling" && <Udvikling />}
       <p>{description}</p>
     </article>
   )
